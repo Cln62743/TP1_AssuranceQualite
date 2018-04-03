71e6d79ad90e9998f596415ee3a56bad948c42b8
philippe chevry <phil_chevry@hotmail.com>
2018-04-03 15:25:34 -0400

    ajout de fiche de test philippe chevry et corection pour les test de philippe chevry

--- TP1_AssuranceQualite/src/Test/testPhilippeChevry.java
+++ TP1_AssuranceQualite/src/Test/testPhilippeChevry.java
@@ -1,5 +1,6 @@
 package Test;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
@@ -13,38 +14,22 @@ class testPhilippeChevry {
 
 
 	@Test
-	void testListeAffichageAvecErreur() {
-		 String[] clientsM1 = {"7","6" ,"5"} ;
-		String fichier1 = "";
-		 double prixM1 =   43.33;
-		 ArrayList<String>erreur1 = new ArrayList<>() ;
-		 erreur1.add("format incorrect");
-		Facture facture = new Facture(fichier1, clientsM1,  prixM1, erreur1);
-		facture.rentrerVariableList();
-		if (facture.rentrerVariableList().size() >= 1) {
-			assertEquals(1, facture.rentrerVariableList().size());
-		}
+	void testClientsNull() {
+		Calcul calcul1 = new Calcul(null);
+		assertNull(calcul1.clients);
 	}
 	
 	@Test
 	void testCommandeNull() {
 	
-	Calcul calcul = new Calcul();
+	Calcul calcul = new Calcul(null);
 			assertNull(calcul.getCommandes());	
 	}
 
 	@Test
-	void testTableauErreurNull() {
-		String fichier = "";
-		 String[] clientsM10 = {"4","2" ,"1"} ;
-			
-		 double prixM10 = 15.45;
-		 
-		 ArrayList<String>erreur10 = new ArrayList<>() ;
-		 erreur10.add(" ");
-		Facture facture2 = new Facture(fichier, clientsM10,  prixM10, erreur10);
-		facture2.erreurCommande = null;
-		assertNull(facture2.erreurCommande);
+	void testBooleanOuvertureFichier() {
+		Calcul calcul2 = new Calcul(null);
+		assertTrue(calcul2.ouvertureFichier());	
 	}
 
 }
